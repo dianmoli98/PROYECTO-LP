0af40ce079f4bc8b3c96c8c24ffc48e84fc0dfb9
Marlon-Lindao-ZX <melindao@espol.edu.ec>
2020-07-11 09:47:45 -0500

    Modificando all

--- sintacticoNewLang.py
+++ sintacticoNewLang.py
@@ -2,14 +2,20 @@ import ply.yacc as yacc
 import lexico_NewLang as mylexer
 tokens = mylexer.tokens
 
-#All
-def p_all(p):
-    '''all : statement
+
+#Statement(1 linea de codigo)
+def p_statement(p):
+    '''statement : statement_value
+    | statement_value POINTCOMMA'''
+    p[0] = p[1]
+
+
+def p_statement_value(p):
+    '''statement_value : expression
     | concatenate
     | declare
     | assign'''
     p[0] = p[1]
-
 
 
 #Declaration of Variables
@@ -20,50 +26,60 @@ def p_declare(p):
     | declare_generic'''
     p[0] = p[1]
 
+
 #Declaration of var_number
 def p_var_number(p):
     'var_number : declare_number EQUAL number_value'
     p[0] = 110
 
+
 def p_declare_number(p):
     'declare_number : declare_any TWOPOINTS VARNUMBER'
 
+
 def p_number_value(p):
     'number_value : expression'
+
 
 #Declaration of var boolean
 def p_var_boolean(p):
     'var_boolean : declare_boolean EQUAL boolean_value'
     p[0] = 120
 
+
 def p_declare_boolean(p):
     'declare_boolean : declare_any TWOPOINTS VARBOOLEAN'
+
 
 def p_declare_boolean_value(p):
     '''boolean_value : boolean
     | variable'''
+
 
 #Declaration of var_string
 def p_var_string(p):
     'var_string : declare_string EQUAL string_value'
     p[0] = 130
 
+
 def p_declare_string(p):
     'declare_string : declare_any TWOPOINTS VARSTRING'
+
 
 def p_string_value(p):
     '''string_value : string
     | variable'''
+
 
 #General Declarator when data type is not specified
 def p_declare_generic(p):
     'declare_generic : declare_any EQUAL assign_value'
     p[0] = 100
 
+
 #Can be used for all declarations
 def p_declare_any(p):
     'declare_any : prefix VARIABLE'
-
 
 
 #Assignments
@@ -71,11 +87,11 @@ def p_assign(p):
     'assign : VARIABLE EQUAL assign_value'
     p[0] = 50
 
+
 def p_assign_value(p):
     '''assign_value : expression
     | boolean
     | string'''
-
 
 
 #ConcatenationString--Review
@@ -83,24 +99,21 @@ def p_concatenate(p):
     'concatenate : termS'
     p[0] = 300
 
-def p_concatenate_termS(p):
+
+def p_concatenate_term(p):
     'termS : string PLUS chain'
     p[0] = 1.0
+
 
 def p_chain(p):
     '''chain : termS PLUS value
     | value'''
 
+
 def p_value(p):
     '''value : expression
     | string'''
 
-
-
-#Statement
-def p_statement_expression(p):
-    'statement : expression'
-    p[0] = p[1]
 
 #Math Operations
 def p_expression_plus(p):
@@ -117,6 +130,7 @@ def p_expression_term(p):
     'expression : term'
     p[0] = p[1]
 
+
 def p_term_product(p):
     'term : term PRODUCT term'
     p[0] = p[1] * p[3]
@@ -127,11 +141,13 @@ def p_term_div(p):
     if p[3] != 0:
         p[0] = p[1] / p[3]
 
+
 def p_term_factor(p):
     '''term : number
     | factor_expr
     | variable'''
     p[0] = p[1]
+
 
 def p_factor_expr(p):
     'factor_expr : LPAREN expression RPAREN'
@@ -145,21 +161,26 @@ def p_prefix(p):
     | STATIC
     | CONST'''
 
+
 def p_number(p):
     '''number : NUMBER
     | FLOAT'''
     p[0] = float(p[1])
 
+
 def p_string(p):
     'string : NORMSTRING1'
+
 
 def p_boolean_value(p):
     '''boolean : TRUE
     | FALSE'''
 
+
 def p_variable(p):
     'variable : VARIABLE'
     p[0] = 1
+
 
 # Error rule for syntax errors
 def p_error(p):
